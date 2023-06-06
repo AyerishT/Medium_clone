b7fba286595056abfa877251dcaaee9412523583
ayerish <ayerish1842.be20@chitkara.edu.in>
2023-06-06 23:30:38 +0530

    first commit

---
+++ backend/db.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+
+const url =
+  "mongodb+srv://ayerish:ayerish@cluster0.el1sedd.mongodb.net/";
+module.exports.connect = () => {
+  mongoose
+    .connect(url, {
+      useNewUrlParser: true,
+      // useFindAndModify: false,
+      useUnifiedTopology: true,
+      // useCreateIndex: true,
+    })
+    .then(() => console.log("MongoDB is connected successfully"))
+    .catch((err) => console.log("Error: ", err));
+};
